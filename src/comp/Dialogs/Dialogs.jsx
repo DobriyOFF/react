35028bcbfde03a46519c889d675aac90b0401b71
Сергей Дорофеев <dobriyofficial@iMac-Sergej.local>
2021-07-30 17:46:13 +0300

    level 22

--- src/comp/Dialogs/Dialogs.jsx
+++ src/comp/Dialogs/Dialogs.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsPerson}>
                 <ul className={s.dialogsList}>
-                    <li className={s.item + ' ' + s.active}>Sergey</li>
+                    <li className={s.item + ' ' + s.active}><NavLink to="/dialogs/1">Sergey</NavLink></li>
+                    <li className={s.item + ' ' + s.active}><NavLink to="/dialogs/2">Petya</NavLink></li>
+                    <li className={s.item + ' ' + s.active}><NavLink to="/dialogs/3">Maxim</NavLink></li>
                 </ul>
             </div>
             <div className={s.messages}>
